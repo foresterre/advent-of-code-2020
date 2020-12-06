45cad37559848b5503adcfbde0b8e46701704446
Martijn Gribnau <garm@ilumeo.com>
2020-12-06 02:40:40 +0100

    Add common read file function

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 use thiserror::Error;
 use std::path::{Path};
 use std::fs::File;
-use std::io::{BufReader, BufRead};
+use std::io::{BufReader, BufRead, Read};
 
 pub type TResult<T> = std::result::Result<T, AdventError>;
 
@@ -17,5 +17,15 @@ pub enum AdventError {
 pub fn lines(path: impl AsRef<Path>) -> TResult<impl Iterator<Item=String>> {
     File::open(path)
         .map(|f| BufReader::new(f).lines().filter_map(|f| f.ok()))
+        .map_err(AdventError::FileError)
+}
+
+pub fn read_file(path: impl AsRef<Path>) -> TResult<String> {
+    File::open(path)
+        .and_then(|f| {
+            let mut content = String::new();
+            BufReader::new(f).read_to_string(&mut content)
+                .map(|_| content)
+        })
         .map_err(AdventError::FileError)
 }
